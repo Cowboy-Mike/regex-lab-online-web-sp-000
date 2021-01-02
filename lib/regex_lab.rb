a46941886b8da441a10176002ae6dc3f948fda74
Mike Nanto <mike@thecowboymechanic.com>
2021-01-02 05:10:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,8 +9,8 @@ end
    
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un+\w\b/)
-  text.scan(/\w+th/)
+  list = text.scan(/un+\w\b/)
+  text.scan(/\w+ing)
 end
 
 def words_five_letters_long(text)
